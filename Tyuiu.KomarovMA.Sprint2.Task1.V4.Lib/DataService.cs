ee6790250786d9ff9799a83887290aff4c881606
Tyuiu.KomarovMA@outlook.com <Tyuiu.KomarovMA@outlook.com>
2023-10-15 20:54:37 +0500

    Task1

---
+++ Tyuiu.KomarovMA.Sprint2.Task1.V4.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.KomarovMA.Sprint2.Task1.V4.Lib
+{
+    public class DataService : ISprint2Task1V4
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = new bool[6];
+
+            res[0] = (a > b) | (c > d + 1); // f f = f
+            res[1] = (a < b) & (c > d + 1); // t f = f
+            res[2] = (a > b) || (c > d + 1);// f f = f
+            res[3] = (a > b) && (c < d + 1); //
+            res[4] = !res[0]; //
+            res[5] = (a > b) ^ (c > d + 1); //
+
+            return res;
+
+        }
+    }
+}
---
+++ Tyuiu.KomarovMA.Sprint2.Task1.V4.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KomarovMA.Sprint2.Task1.V4.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KomarovMA.Sprint2.Task1.V4.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("46bc70b1-99cc-4da3-923e-2a0218c70223")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
